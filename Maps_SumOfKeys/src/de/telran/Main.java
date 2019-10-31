c16b0167825383d5d0f1b520089ad0843bd9f799
david <david_miller1@outlook.de>
2019-10-31 16:40:58 +0100

    1.Дана Map<String, String> map, написать функцию, которая вернет Мап, такую,если в есходной map есть ключ ‘a’ и ‘b’, то нужно создать новый ключ ‘ab’ ссуммой значений от ключей a и b.Примеры:mapAB({"a": "Hi", "b": "There"}) → {"a": "Hi", "ab": "HiThere", "b": "There"}mapAB({"a": "Hi"}) → {"a": "Hi"}mapAB({"b": "There"}) → {"b": "There"} + 2.Дан массив букв, вернуть мапу Map<String, Boolean> где каждая строкаявляется ключем, а значением true, если строка в массиве больше одного разаи false, если только один раз.Примеры:wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}wordMultiple(["c", "c", "c", "c"]) → {"c": true}

--- Maps_SumOfKeys/src/de/telran/Main.java
+++ Maps_SumOfKeys/src/de/telran/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         Map<String, String> m1 = new HashMap<>();
-        m1.put("a", "Hi");
-        m1.put("b", "There");
+        m1.put("a", "Hi ");
+        m1.put("b", "There ");
         String[] str = {"a", "b", "f", "c", "d", "e", "c", "f"};
 
         System.out.println(getAB(m1));
